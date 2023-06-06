ed615548b780ebcf7fa83afd0bd08c54ef55704b
MineKnight2711 <witherdragonlair@gmail.com>
2023-06-06 13:51:52 +0700

    changes

--- lib/controller/get_ip.dart
+++ lib/controller/get_ip.dart
@@ -4,10 +4,10 @@ Future<InternetAddress> getCurrentIPv4Address() async {
   for (var interface in await NetworkInterface.list()) {
     print(interface.name);
     for (var address in interface.addresses) {
-      Check if the address is IPv4 and not a loopback address
-      if (address.type == InternetAddressType.IPv4 && !address.isLoopback) {
-        return address;
-      }
+      // Check if the address is IPv4 and not a loopback address
+      // if (address.type == InternetAddressType.IPv4 && !address.isLoopback) {
+      //   return address;
+      // }
       if (address.type == InternetAddressType.IPv4 && !address.isLoopback) {
         return address;
       }
